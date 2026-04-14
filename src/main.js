20bb8d12d944df019660e121e21bed0a3934293f
JMeltzer92 <jmeltze1@umd.edu>
2026-04-14 19:39:41 -0400

    setting up vue sandbox

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
 import './styles.css'
 
-const app = document.getElementById('app')
-
-function render() {
-	const now = new Date().toLocaleTimeString()
-	document.getElementById('foo').textContent = `The current time is ${now}.`
-}
-
-render()
-
-// Hot-reload demo: update time every second in dev.
-setInterval(render, 1000)
+createApp(App).mount('#app')
